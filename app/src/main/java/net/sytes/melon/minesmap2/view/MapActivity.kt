abe3446ec7b69dc50eeb67500b489f211b3f57ea
mob-GitHub <halu.nomi@gmail.com>
2019-09-21 01:04:28 +0900

    Gradle等のアップデート
    ・AndroidXへ変更
    ・Gradleアップデート
    ・Kotlinアップデート
    ・ビルドターゲットを29(Android10)へ変更
    ・GoogleDriveへの自動バックアップを無効化

--- app/src/main/java/net/sytes/melon/minesmap2/view/MapActivity.kt
+++ app/src/main/java/net/sytes/melon/minesmap2/view/MapActivity.kt
@@ -1,6 +1,6 @@
 package net.sytes.melon.minesmap2.view
 
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import net.sytes.melon.minesmap2.R
 
